34d65731e82ca93e14b48963f504c4b1ddb20d4d
sourav <sourav@email.com>
2026-01-29 12:57:13 +0530

    module 2 exercises completed

--- MetaDBECapstoneModule2.sql
+++ MetaDBECapstoneModule2.sql
@@ -145,3 +145,63 @@ DELIMITER ;
 
 
 CALL AddValidBooking('2022-10-13', '2');
+
+
+-- Fourth Exercise
+-- Task 1
+DELIMITER //
+CREATE PROCEDURE AddBooking(IN bkgID INT, IN bkgDate DATE, IN tblNumber CHAR(1), IN custID INT)
+BEGIN
+    SET @tblCount = (
+        SELECT COUNT(*) FROM bookings WHERE BookingID = bkgID
+    );
+
+    START TRANSACTION;
+    INSERT INTO bookings (BookingID, BookingDate, TableNumber, CustomerID)
+    VALUES (bkgID, bkgDate, tblNumber, custID);
+
+    IF @tblCount = 0 THEN
+        COMMIT;
+        SELECT 'New booking added' AS `Confirmation`;
+    ELSE
+        ROLLBACK;
+        SELECT 'Booking already exists' AS `Confirmation`;
+    END IF;
+END//
+DELIMITER ;
+
+
+CALL AddBooking(9, '2022-12-30', 3, 4);
+
+
+-- Task 2
+DELIMITER //
+CREATE PROCEDURE UpdateBooking(IN bkgID INT, IN bkgDate DATE)
+BEGIN
+    START TRANSACTION;
+    UPDATE bookings
+    SET BookingDate = bkgDate
+    WHERE BookingID = bkgID;
+    COMMIT;
+
+    SELECT CONCAT('Booking ', bkgID, ' is updated') AS `confirmation`;
+END//
+DELIMITER ;
+
+CALL UpdateBooking(9, '2022-07-18');
+
+-- Task 3
+
+DELIMITER //
+CREATE PROCEDURE CancelBooking(IN bkgID INT)
+BEGIN
+    START TRANSACTION;
+    DELETE FROM bookings
+    WHERE BookingID = bkgID;
+    COMMIT;
+
+    SELECT CONCAT('Booking ', bkgID, ' is cancelled') AS `confirmation`;
+END//
+DELIMITER ;
+
+CALL CancelBooking(9);
